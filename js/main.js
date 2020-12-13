3aae7ece9b1db911b14e66307644ffea32a3416d
Mathiazi <lucas-mathiazi@bol.com.br>
2020-12-13 03:06:06 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,133 +1,3 @@
-function FINAL ()
-{
-    const cod = document.getElementById("ITEM").value;
-    
-    const tamanho = cod.length;
-
-    var digito;
-
-    if (tamanho == 6)
-    {
-        digito = Number(cod.substr(1, 1)) + 
-                    Number(cod.substr(3, 1)) + 
-                        Number(cod.substr(5, 1));
-                        
-        digito = (digito * 3) + Number(cod.substr(0, 1)) +
-                                    Number(cod.substr(2, 1)) +
-                                        Number(cod.substr(4, 1));
-        digito = 10 - (digito % 10);
-
-        if (digito == 10)
-        {
-            digito = 0;
-        }
-    }
-    else if (tamanho == 9)
-    {
-        digito =  Number(cod.substr(0, 1)) + 
-                    Number(cod.substr(2, 1)) + 
-                        Number(cod.substr(4, 1)) + 
-                            Number(cod.substr(6, 1)) + 
-                                Number(cod.substr(8, 1));
-
-        digito = (digito * 3) + Number(cod.substr(1, 1)) +
-                                    Number(cod.substr(3, 1)) +
-                                        Number(cod.substr(5, 1)) +
-                                            Number(cod.substr(7, 1));
-        digito = 10 - (digito % 10);
-
-        if (digito == 10)
-        {
-            digito = 0;
-        }
-    }
-    else {
-        digito = '?';
-    }
-
-    document.getElementById("BOTAO1").innerHTML = digito;
-}
-
-var posicao;
-
-function PRECO () {
-
-    var IMG = "https://cutt.ly/mathi" + posicao;
-
-    if (posicao => 0 && posicao <= 50)
-    {
-        document.getElementById("FOTO").src = IMG;
-            document.getElementById("LINK").href = IMG;
-    }
-
-    FINAL();
-}
-
-function CODIGO () {
-
-    const cod = [
-        /*00*/'157427',
-        /*01*/'980008473',
-        /*02*/'722449',
-        /*03*/'131592',
-        /*04*/'321994',
-        /*05*/'226263',
-        /*06*/'980012473',
-        /*07*/'091962',
-        /*08*/'980014286',
-        /*09*/'631629',
-        /*10*/'752168',
-        /*11*/'840908',
-        /*12*/'980008232',
-        /*13*/'310746',
-        /*14*/'980008161',
-        /*15*/'980003562',
-        /*16*/'623681',
-        /*17*/'177614',
-        /*18*/'177607',
-        /*19*/'226151',
-        /*20*/'166645',
-        /*21*/'386711',
-        /*22*/'227853',
-        /*23*/'226158',
-        /*24*/'628106',
-        /*25*/'980007145',
-        /*26*/'372529',
-        /*27*/'980004676',
-        /*28*/'352107',
-        /*29*/'980004882',
-        /*30*/'980014772'
-    ];
-
-    var status = document.getElementById("BOTAO2");
-
-    const item_selecionado = document.getElementById("ITENS").value;
-          
-    if (item_selecionado == "")
-    {
-        status.innerHTML = "?";
-    }
-    else
-    {
-        posicao = Number(item_selecionado.substr(0, 2));
-        
-        const verifica = cod[posicao];
-    
-        if (verifica == undefined)
-        {
-            status.innerHTML = "-";
-        }
-        else
-        {
-            status.innerHTML = ">";
-
-            document.getElementById("ITEM").value = verifica;
-    
-            PRECO();
-        }
-    }
-}
-
 function CARREGAR_LISTA () {
 
     const itens = [
@@ -191,6 +61,138 @@ function BLOQUEIO (e) {
         if (charCode < 48 || charCode > 57)
         {
             return false;
+        }
+    }
+}
+
+var posicao;
+
+function PRECO () {
+
+    if (posicao <= 30)
+    {
+        var carregar_imagem = "https://cutt.ly/item-" + posicao;
+
+        document.getElementById("FOTO").src = carregar_imagem;
+        
+        document.getElementById("LINK").href = carregar_imagem;
+
+    }
+}
+
+function FINAL ()
+{
+    const cod = document.getElementById("ITEM").value;
+    
+    const tamanho = cod.length;
+
+    var digito;
+
+    if (tamanho == 6)
+    {
+        digito = Number(cod.substr(1, 1)) + 
+                    Number(cod.substr(3, 1)) + 
+                        Number(cod.substr(5, 1));
+                        
+        digito = (digito * 3) + Number(cod.substr(0, 1)) +
+                                    Number(cod.substr(2, 1)) +
+                                        Number(cod.substr(4, 1));
+        digito = 10 - (digito % 10);
+
+        if (digito == 10)
+        {
+            digito = 0;
+        }
+    }
+    else if (tamanho == 9)
+    {
+        digito =  Number(cod.substr(0, 1)) + 
+                    Number(cod.substr(2, 1)) + 
+                        Number(cod.substr(4, 1)) + 
+                            Number(cod.substr(6, 1)) + 
+                                Number(cod.substr(8, 1));
+
+        digito = (digito * 3) + Number(cod.substr(1, 1)) +
+                                    Number(cod.substr(3, 1)) +
+                                        Number(cod.substr(5, 1)) +
+                                            Number(cod.substr(7, 1));
+        digito = 10 - (digito % 10);
+
+        if (digito == 10)
+        {
+            digito = 0;
+        }
+    }
+    else {
+        digito = '?';
+    }
+
+    document.getElementById("BOTAO1").innerHTML = digito;
+
+    PRECO();
+}
+
+function CODIGO () {
+
+    const cod = [
+        /*00*/'157427',
+        /*01*/'980008473',
+        /*02*/'722449',
+        /*03*/'131592',
+        /*04*/'321994',
+        /*05*/'226263',
+        /*06*/'980012473',
+        /*07*/'091962',
+        /*08*/'980014286',
+        /*09*/'631629',
+        /*10*/'752168',
+        /*11*/'840908',
+        /*12*/'980008232',
+        /*13*/'310746',
+        /*14*/'980008161',
+        /*15*/'980003562',
+        /*16*/'623681',
+        /*17*/'177614',
+        /*18*/'177607',
+        /*19*/'226151',
+        /*20*/'166645',
+        /*21*/'386711',
+        /*22*/'227853',
+        /*23*/'226158',
+        /*24*/'628106',
+        /*25*/'980007145',
+        /*26*/'372529',
+        /*27*/'980004676',
+        /*28*/'352107',
+        /*29*/'980004882',
+        /*30*/'980014772'
+    ];
+
+    var status = document.getElementById("BOTAO2");
+
+    const item_selecionado = document.getElementById("ITENS").value;
+          
+    if (item_selecionado == "")
+    {
+        status.innerHTML = "?";
+    }
+    else
+    {
+        posicao = Number(item_selecionado.substr(0, 2));
+        
+        const verifica = cod[posicao];
+    
+        if (verifica == undefined)
+        {
+            status.innerHTML = "-";
+        }
+        else
+        {
+            status.innerHTML = "";
+
+            document.getElementById("ITEM").value = verifica;
+    
+            FINAL();
         }
     }
 }
