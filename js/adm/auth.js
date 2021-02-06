464d7c3e736de83cce994b2b4f51890fc7788b12
Mathiazi <lucas-mathiazi@bol.com.br>
2021-02-06 04:38:39 -0300

    clean code

--- js/adm/auth.js
+++ js/adm/auth.js
@@ -1,18 +1,20 @@
 function closeAuth() {
-    let formLogin = document.getElementById("form-login");
-    formLogin.style.display = "none";
+    setTimeout(() => {
+        var formLogin = document.getElementById("form-login");
+        formLogin.style.display = "none";
+    }, 200)
 }
 function admLoginDesktop() {
-    setTimeout( () => {
+    setTimeout(() => {
         let formLogin = document.getElementById("form-login");
         formLogin.style.display = 'block';
-    }, 100)
+    }, 500)
 }
 function admLoginMob() {
     menuMobile();
 
-    setTimeout( () => {
+    setTimeout(() => {
         let formLogin = document.getElementById("form-login");
         formLogin.style.display = 'block';
-    }, 500)
+    }, 1000)
 }
